b90606ad8d10ac068d6697b05da9dcd0143027bf
RArbore <russel.jma@gmail.com>
2021-05-08 20:49:46 -0700

    Colored window

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"runtime"
 	"github.com/go-gl/glfw/v3.3/glfw"
+	"github.com/go-gl/gl/v4.6-core/gl"
 )
 
+func render() {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+}
+
 func main() {
-runtime.LockOSThread()
+	runtime.LockOSThread()
 	if err := glfw.Init(); err != nil {
 		panic(fmt.Errorf("could not initialize glfw: %v", err))
 	}
@@ -24,7 +29,14 @@ runtime.LockOSThread()
 	}
 
 	win.MakeContextCurrent()
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	gl.ClearColor(0.25, 0.265, 0.29, 1.0)
+
 	for !win.ShouldClose() {
+		render()
 		win.SwapBuffers()
 		glfw.PollEvents()
 	}

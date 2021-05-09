8fc551bac3a14252d3021eec077822a14cf7938f
RArbore <russel.jma@gmail.com>
2021-05-08 20:31:52 -0700

    Black window

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"github.com/go-gl/glfw/v3.3/glfw"
 )
 
 func main() {
-	fmt.Println("Hello world.")
+runtime.LockOSThread()
+	if err := glfw.Init(); err != nil {
+		panic(fmt.Errorf("could not initialize glfw: %v", err))
+	}
+
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.Resizable, glfw.True)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	win, err := glfw.CreateWindow(800, 600, "Hello world", nil, nil)
+	if err != nil {
+		panic(fmt.Errorf("could not create opengl renderer: %v", err))
+	}
+
+	win.MakeContextCurrent()
+	for !win.ShouldClose() {
+		win.SwapBuffers()
+		glfw.PollEvents()
+	}
 }
